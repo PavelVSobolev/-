a500de7dc1a101e4fdad3715e38baf1e396422d6
PavelVSobolev <pavelsv69595839@gmail.com>
2024-08-27 14:48:48 +0300

    Updated task1.py

--- for hp lab/task1/task1.py
+++ for hp lab/task1/task1.py
@@ -1,11 +1,28 @@
-n, m = map(int,input().split())
-path = ""
-fn = 1
-while True:
-    path += str(fn)
-    fn = 1 + (fn - 2 + m) % n
-    if fn == 1:
-        break
-print(path)
+
+import argparse
+
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('name1', nargs='?')
+    parser.add_argument ('name2', nargs='?')
+
+    return parser
+
+
+if __name__ == '__main__':
+    parser = createParser()
+    namespace = parser.parse_args()
+
+    n = int(namespace.name1)
+    m = int(namespace.name2)
+
+    path = ""
+    fn = 1
+    while True:
+        path += str(fn)
+        fn = 1 + (fn - 2 + m) % n
+        if fn == 1:
+            break
+    print(path
 
 
