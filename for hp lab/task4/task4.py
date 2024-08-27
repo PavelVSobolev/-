8ebca9c464d73d135a3e25ea1bc7e83966068aa3
PavelVSobolev <pavelsv69595839@gmail.com>
2024-08-27 15:09:51 +0300

    task4.py

--- for hp lab/task4/task4.py
+++ for hp lab/task4/task4.py
@@ -1,14 +1,27 @@
-filename = input("Введите путь к файлу: ")
-nums = []
-file_in = open(filename, "r")
-for line in file_in:
-    nums.append(int(line.rstrip("\n")))
-file_in.close()
+import argparse
 
-def EQ(nums):
-    nums.sort()
-    med = int(nums[len(nums) // 2])
-    moves = sum(abs(n - med) for n in nums)
-    return moves
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('name1', nargs='?')
 
-print(EQ(nums))
+    return parser
+
+
+if __name__ == '__main__':
+    parser = createParser()
+    namespace = parser.parse_args()
+    filename1 = namespace.name1
+
+    nums = []
+    file_in = open(filename1, "r")
+    for line in file_in:
+        nums.append(int(line.rstrip("\n")))
+    file_in.close()
+
+    def EQ(nums):
+        nums.sort()
+        med = int(nums[len(nums) // 2])
+        moves = sum(abs(n - med) for n in nums)
+        return moves
+
+    print(EQ(nums))
